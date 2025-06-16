fb5705d3890cb132639a1d5b8d43f78ca1dd98cf
Aymen Sakouhi <aymenmarketer@gmail.com>
2025-06-17 01:52:06 +0200

    writing a card tool to zoom in on hover for images

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import "./App.css";
+import Card from "./componetns/Card";
+import PAINTINGS from "./data/museum.json";
 
 const App = () => {
   return (
-    <div className="font-museomoderno">
-      <h1 className="text-3xl font-bold underline">Rsbuild with React</h1>
-      <p>Start building amazing things with Rsbuild.</p>
+    <div className="font-museomoderno bg-slate-300 pt-2 px-8">
+      <h1 className="text-6xl font-bold text-center text-slate-600 py-4">
+        Moderno Museum
+      </h1>
+      {PAINTINGS.map((painting) => (
+        <Card key={Math.random().toString(26).slice(2)} painting={painting} />
+      ))}
     </div>
   );
 };
--- src/componetns/Card.tsx
+++ src/componetns/Card.tsx
@@ -1,7 +1,56 @@
-import React from "react";
+import * as React from "react";
 
-const Card = () => {
-  return <div>Card</div>;
+type CardProps = {
+  painting: {
+    url: string;
+    title: string;
+    description: string;
+  };
+};
+
+const Card = ({ painting }: CardProps) => {
+  const imgRef = React.useRef<HTMLImageElement>(null);
+
+  const onImageHover = (e: React.MouseEvent<HTMLImageElement>) => {
+    if (imgRef.current) {
+      const boundingRect = imgRef.current?.getBoundingClientRect();
+      const xPosition = e.clientX - boundingRect?.left;
+      const yPosition = e.clientY - boundingRect?.top;
+
+      const xPercent = (xPosition / boundingRect?.width) * 100;
+      const yPercent = (yPosition / boundingRect?.height) * 100;
+
+      imgRef.current.style.transformOrigin = `${xPercent}% ${yPercent}%`;
+      imgRef.current.style.transform = "scale(1.2)";
+    }
+  };
+
+  const onImageLeave = () => {
+    const img = imgRef.current;
+    if (img) {
+      img.style.transform = "scale(1)";
+      img.style.transformOrigin = "center";
+    }
+  };
+
+  return (
+    <div className="grid xs:grid-cols-1 md:grid-cols-2 gap-4 my-4">
+      <div className="border-2 rounded-md overflow-hidden">
+        <img
+          ref={imgRef}
+          src={painting.url}
+          alt={painting.title}
+          onMouseMove={onImageHover}
+          onMouseLeave={onImageLeave}
+          className="w-full h-full object-cover hover:scale-120 transition-transform duration-300"
+        />
+      </div>
+      <div className="border-2 rounded-md overflow-hidden p-4 text-2xl flex flex-col justify-center items-center">
+        <span className="mb-4 text-4xl">{painting.title}</span>
+        <p className="text-justify">{painting.description}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
